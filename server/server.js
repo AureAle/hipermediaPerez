e2b5f77a0324e18fd61061ddeae07766d258252c
AureAle <aperez33@ucol.mx>
2019-08-23 08:32:04 -0500

    server heroku

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log('Port 3000');
+});
+
