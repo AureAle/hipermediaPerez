b9bda8dbc63917aaa6615a1bedeba96b91b362fe
AureAle <aperez33@ucol.mx>
2019-08-30 07:31:20 -0500

    Bye forever

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Bye bye World!');
+  res.send('Bye forever World!');
 });
 
 const PORT = process.env.PORT || 3000;
